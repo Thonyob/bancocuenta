96a49ec5ef3d7046ae1b7d88d91a7339f8219ff6
Thonyob <anthonyolivaresb@gmail.com>
2023-05-19 20:24:45 -0500

    primer commit

---
+++ src/main/java/com/banco/cuenta/bancocuenta/BancoCuentaApplication.java
@@ -0,0 +1,13 @@
+package com.banco.cuenta.bancocuenta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoCuentaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BancoCuentaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/banco/cuenta/bancocuenta/BancoCuentaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banco.cuenta.bancocuenta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoCuentaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
